b746bc168ae0f8005d179b2a985db87143b53d31
unknown <duongchidu30072005@gmail.com>
2025-04-25 10:38:54 +0700

    Lesson01

---
+++ DcdLesson01Mvc/Controllers/DcdHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DcdLesson01Mvc.Controllers
+{
+    public class DcdHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
